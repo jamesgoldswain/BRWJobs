480ff2edba52f4ab80f86d1d709bbdb6fbfef13f
jamesgoldswain <james@ouimedia.com>
2016-02-22 20:36:04 +0800

    added index file, db schema and serverjs

--- brw_scraper.js
+++ brw_scraper.js
@@ -1,4 +1,4 @@
-var request =  
+var request =
     require("request"),
     fs = require('fs'),
     jsonfile = require('jsonfile'),
@@ -24,15 +24,55 @@ request(url, function (error, response, body) {
       });
       // save to db
 		console.log(companies);
-        
+
         var file = 'data/companies.json'
         var obj = {companies : companies}
-        
-        jsonfile.writeFile(file, obj, function (err) {
-            console.error(err)
-        })
+
+        save(obj);
+        //jsonfile.writeFile(file, obj, function (err) {
+        //    console.error(err)
+        //})
 
 	} else {
 		console.log("We’ve encountered an error: " + error);
 	}
 });
+
+
+var save = function(doc){
+    //lets require/import the mongodb native drivers.
+  var mongodb = require('mongodb');
+
+  //We need to work with "MongoClient" interface in order to connect to a mongodb server.
+  var MongoClient = mongodb.MongoClient;
+
+  // Connection URL. This is where your mongodb server is running.
+  var url = 'mongodb://localhost:27017/brwjobs';
+
+  // Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      //HURRAY!! We are connected. :)
+      console.log('Connection established to', url);
+
+      // Get the documents collection
+      var collection = db.collection('results');
+
+      //Create some users
+      //var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+
+      // Insert some users
+      collection.insert(doc, function (err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Inserted %d documents into the "results" collection. The documents inserted with "_id" are:', result.length, result);
+        }
+        //Close connection
+        db.close();
+      });
+    }
+  });
+}
